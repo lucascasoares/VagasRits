aa427b571141b2612ded4a072539be0218cebbb1
Lucas Soares <lucascasoares@gmail.com>
2020-03-05 17:59:09 -0300

    Create the basis for the administration login
    Just creating the login files, with the command docker-compose run web php artisan ui: auth

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 
 Route::get('candidates', 'CandidatesSiteController@create');
 Route::post('candidates/store', 'CandidatesSiteController@store');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
